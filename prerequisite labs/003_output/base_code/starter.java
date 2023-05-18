8388ce1b3d5dd8bf47c32094904b6d67f31fc9b4
gad.maj <gadymajor@gmail.com>
2023-05-18 14:27:31 -0700

    initial Commit

---
+++ prerequisite labs/003_output/base_code/starter.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+class starter {
+	public static void main(String args[]) {
+		System.out.print("Hello my name is daryl"); 
+	}
+}
---
+++ prerequisite labs/018_while_loops_example/base_code/starter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class starter {
+	public static void main(String args[]) {
+		// Your code goes below here
+		int daryl = 0;
+		while(daryl<460){
+			System.out.println("Fungus");
+			daryl = daryl * daryl + 20;
+		}
+
+
+		
+	}
+}
